75786760a83b48dea4ccb58d942848d69c2a2686
Trapper Daigle <tdaigle@WyoMovies.com>
2016-06-09 16:37:20 -0600

    Test

---
+++ TestProject/TestProject.WebServer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestProject.WebServer.Startup))]
+namespace TestProject.WebServer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
